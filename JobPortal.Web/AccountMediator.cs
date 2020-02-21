6918c833d122f4398eee5ac75c0082e4787a2579
Ilakiyasaravanan <58771810+Ilakiyasaravanan@users.noreply.github.com>
2020-02-21 18:51:54 +0530

    Add files via upload

---
+++ JobPortal.Web/AccountMediator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JobPortal.DAL;
+using JobPortal.Entity;
+namespace JobPortal.Web
+{
+    public class AccountMediator
+    {
+        public AccountMediator() { 
+        }
+        public bool AddAccountDetails(AccountDetails account)
+        {
+            bool status = new AccountRepository().Add(account);
+            return status;
+        }
+        public bool CheckAccountDetails(AccountDetails acc)
+        {
+            bool loginStatus = new AccountRepository().Check(acc);
+            return loginStatus;
+        }
+    }
+}
